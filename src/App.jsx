08a3b65f891427adc560284c16ff1c24d730eb42
AlexandruStefanGherhes <104617757+AlexandruStefanGherhes@users.noreply.github.com>
2023-10-07 18:15:20 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Hello from "./Hello"
+const data = 'wow so much code'
+
+
+function App() {
+  return (
+      <div>
+        <Hello data={data} age={'20'}/>     
+      </div>
+  )
+}
+
+export default App
---
+++ src/Hello.jsx
@@ -0,0 +1,9 @@
+const Hello = ({data,age}) =>{
+    return(
+        <div>
+            <p>{data}: {age}</p>
+        </div>
+    )
+}
+
+export default Hello
